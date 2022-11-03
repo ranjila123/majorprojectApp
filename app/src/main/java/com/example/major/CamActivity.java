f1845ee4ac2922f98f8de7394a1387f0e1927710
ranjil123 <ranjilashrestha123.com>
2022-11-03 10:03:23 +0545

    App for major project after adding gallery

--- app/src/main/java/com/example/major/CamActivity.java
+++ app/src/main/java/com/example/major/CamActivity.java
@@ -27,8 +27,9 @@ public class CamActivity extends AppCompatActivity {
 
     private static final int PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_CODE = 1001;
+    private static final int GALLERY_REQ_CODE = 1002;
     Uri imageUri;
-    ActivityResultLauncher<Intent>activityLauncher = registerForActivityResult(
+    ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
@@ -38,56 +39,80 @@ public class CamActivity extends AppCompatActivity {
             }
 
     );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
     }
 
-    public void check(View v){
+    public void check(View v) {
         //permission
-        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.M){
-            if(checkSelfPermission(Manifest.permission.CAMERA)==
-                    PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)==
-                    PackageManager.PERMISSION_DENIED)
-            {
-                String[] permission = {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                requestPermissions(permission,PERMISSION_CODE);
-            }
-            else
-            {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, PERMISSION_CODE);
+            } else {
                 OpenCamera();
             }
-        }
-        else
-        {
+        } else {
             OpenCamera();
         }
     }
 
     private void OpenCamera() {
         ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE,"New Image");
-        values.put(MediaStore.Images.Media.DESCRIPTION,"from the camera");
-        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
+        values.put(MediaStore.Images.Media.TITLE, "New Image");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "from the camera");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         activityLauncher.launch(cameraIntent);
 
     }
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode==RESULT_OK)
-        {
-            Intent i =new Intent(CamActivity.this,ScanActivity.class);
-            i.setData(imageUri);
-            startActivity(i);
 
-            //so we send image uri to next activity ScanActivity
+    public void checkG(View v) {
+        //permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) ==
+                    PackageManager.PERMISSION_DENIED || checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) ==
+                    PackageManager.PERMISSION_DENIED) {
+                String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                requestPermissions(permission, PERMISSION_CODE);
+            } else {
+                OpenGallery();
+            }
+        } else {
+            OpenGallery();
         }
     }
 
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+        galleryIntent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        activityLauncher.launch(galleryIntent);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (resultCode == IMAGE_CAPTURE_CODE) {
+                Intent i = new Intent(CamActivity.this, ScanActivity.class);
+                i.setData(imageUri);
+                startActivity(i);
+                //send uri to scanactivity
+            }
+//
+//            if (resultCode == GALLERY_REQ_CODE) {
+//                Intent i = new Intent(CamActivity.this, ScanActivity.class);
+//                i.setData(imageUri);
+//                startActivity(i);
+//            }
+
+        }
+    }
 }
